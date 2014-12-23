c71af55d872ce55305e7adefffffc160f06c97b2
Sourav Das <sourav.das@yottolabs.com>
2014-12-23 15:35:29 +0530

    Updaed Skill Set with validation

--- app/models/skill_set.rb
+++ app/models/skill_set.rb
@@ -2,8 +2,6 @@ class SkillSet < ActiveRecord::Base
   has_one :chat_que
   has_and_belongs_to_many :user
 
-  validates :name,  presence: true
-  
-  validates_format_of :name, with: /\A[^`^="<>|$&]\z/, message: "Not Allows %{value}."
-  #validates_exclusion_of :name, in: %w( $% #% ), message: "%{value} is Not Allowed!"
+  validates :name,  presence: true, format: {with: /\A[a-zA-Z0-9.&-_@!()\s]+\z/, message: "Alphanumeric,.,&,-,_,@,!,(),space Allowed"}
+   
 end
