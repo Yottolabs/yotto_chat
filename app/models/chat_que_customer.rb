198284fe64758292f74bf0344fb82d3d0c65e451
Sourav Das <sourav.das@yottolabs.com>
2014-12-23 18:48:56 +0530

    updated Relation

--- app/models/chat_que_customer.rb
+++ app/models/chat_que_customer.rb
@@ -2,4 +2,7 @@ class ChatQueCustomer < ActiveRecord::Base
   belongs_to :customer
   belongs_to :chat_que
   has_one :conversation
+
+  validates :customer_id,  presence: true
+  validates :cha_que_id,  presence: true
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,6 @@
 class Conversation < ActiveRecord::Base
+  belongs_to :chat_que_customer
+  has_many  :messages
+
+  validates :chat_que_customer_id,  presence: true
 end
