99548c3a8130f80b8a6f57f9f77e5fc48f7d915e
Sourav Das <sourav.das@yottolabs.com>
2014-12-23 18:35:06 +0530

    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223123508) do
+ActiveRecord::Schema.define(version: 20141223125617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20141223123508) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "conversations", force: true do |t|
+    t.integer  "chat_que_customer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "conversations", ["chat_que_customer_id"], name: "index_conversations_on_chat_que_customer_id", using: :btree
 
   create_table "customers", force: true do |t|
     t.string   "name"
