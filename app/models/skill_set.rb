0978353bcfb5296e17f0aacf0b3abd7318cdaa82
Santu Koley <santu.koley@yottolabs.com>
2015-01-08 17:01:24 +0530

    parent_id added

--- app/models/skill_set.rb
+++ app/models/skill_set.rb
@@ -1,7 +1,26 @@
+# == Schema Information
+#
+# Table name: skill_sets
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  parent_id  :integer
+#
+
 class SkillSet < ActiveRecord::Base
   has_one :chat_que
-  has_and_belongs_to_many :users, join_table: :users_skill_sets
+  has_and_belongs_to_many :users
+
+  with_options foreign_key: :parent_id, class_name: 'SkillSet' do |skillset|
+    skillset.belongs_to :parent
+    skillset.has_many   :children
+  end
 
   validates :name,  presence: true, format: {with: /\A[a-zA-Z0-9.&-_@!()\s]+\z/, message: "Alphanumeric,.,&,-,_,@,!,(),space Allowed"}
-   
+
+  scope :base, -> {where(parent_id: nil)}
+ 
+
 end
