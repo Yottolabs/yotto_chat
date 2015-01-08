7cfa7888490d37452b0988e707cde547d2fc3c28
Santu Koley <santu.koley@yottolabs.com>
2015-01-08 17:07:29 +0530

    Dummy Data for Dev DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Creating 5 Base Skill with 5 child skills"
+5.times do
+  _skill_set = SkillSet.create(name: Faker::Name.name)
+  5.times do
+    _first_child = _skill_set.children.build(name: Faker::Name.name)
+    _skill_set.save
+    5.times do
+      _first_child.children.build(name: Faker::Name.name)
+      _first_child.save
+    end
+    
+  end
+  
+end
+
+
+puts "Create 10 User"
+10.times do
+  User.create(
+                name:       Faker::Name.name,
+                email:      Faker::Internet.email,
+                password:   Faker::Internet.password
+            )
+end
+
+puts "Create My User"
+User.create(
+              name:     "Santu Koley",
+              email:    "santu.cst08@gmail.com",
+              password: "santu123"
+            )
