6fefc75d194b53a7a0b2a5b96e4453aa9116ebdb
Santu Koley <santu.koley@yottolabs.com>
2015-01-13 13:06:55 +0530

    parent id added

--- db/migrate/20141223061125_create_skill_sets.rb
+++ db/migrate/20141223061125_create_skill_sets.rb
@@ -2,7 +2,7 @@ class CreateSkillSets < ActiveRecord::Migration
   def change
     create_table :skill_sets do |t|
       t.string :name
-
+      t.integer :parent_id
       t.timestamps
     end
   end
