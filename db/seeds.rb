83cc57c4b56f1781defd058e839980bf1bcf2936
Santu Koley <santu.koley@yottolabs.com>
2015-01-13 13:10:17 +0530

    Seed Updated according Development requirement for Skill Set impl

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
-puts "Creating 5 Base Skill with 5 child skills"
-5.times do
-  _skill_set = SkillSet.create(name: Faker::Name.name)
-  5.times do
-    _first_child = _skill_set.children.build(name: Faker::Name.name)
-    _skill_set.save
-    5.times do
-      _first_child.children.build(name: Faker::Name.name)
-      _first_child.save
-    end
+# puts "Creating 5 Base Skill with 5 child skills"
+# 5.times do
+#   _skill_set = SkillSet.create(name: Faker::Name.name)
+#   5.times do
+#     _first_child = _skill_set.children.build(name: Faker::Name.name)
+#     _skill_set.save
+#     5.times do
+#       _first_child.children.build(name: Faker::Name.name)
+#       _first_child.save
+#     end
     
-  end
+#   end
   
-end
+# end
 
 
-puts "Create 10 User"
+# puts "Create 10 User"
 10.times do
   User.create(
                 name:       Faker::Name.name,
                 email:      Faker::Internet.email,
-                password:   Faker::Internet.password
+                password:   Faker::Internet.password(8)
             )
 end
 
