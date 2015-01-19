aab18410dab57c57933ab6f56067442d423aeae1
Santu Koley <santu.koley@yottolabs.com>
2015-01-19 09:37:52 +0530

    code clean up

--- app/controllers/skill_sets_controller.rb
+++ app/controllers/skill_sets_controller.rb
@@ -12,7 +12,6 @@ class SkillSetsController < ApplicationController
   def new
       @skill_set = SkillSet.new
       @skill_set.build_chat_que
-      #@skill_set.users.build
       @users = User.all
       @base_skill_set = SkillSet.base
       @skill_sets = SkillSet.all
@@ -27,15 +26,14 @@ class SkillSetsController < ApplicationController
       @skill_set.update_attributes(skill_set_users)
       flash[:success] = "Skill Set Added Successfully."
       redirect_to @skill_set
-      # Handle a successful save.
     else
       render 'new'
     end
   end
+
   def edit
     @skill_set = SkillSet.find(params[:id])
     @skill_set.chat_que || @skill_set.build_chat_que
-    #@skill_set.users || @skill_set.users.build
     @users = User.all
     @base_skill_set = SkillSet.base
     @skill_sets = SkillSet.all
@@ -47,11 +45,11 @@ class SkillSetsController < ApplicationController
     if @skill_set.update_attributes(skill_set_params)
       flash[:success] = "Skill Set updated"
       redirect_to @skill_set
-
     else
       render 'edit'
     end
   end
+
   def destroy
     skillset = SkillSet.find(params[:id])
     skillset.destroy
@@ -61,21 +59,13 @@ class SkillSetsController < ApplicationController
 
   private
 
-    def skill_set_params
-      params.require(:skill_set).permit(:name,:parent_id,:user_ids=>[],chat_que_attributes: [:id,:name])
-    end
-
-    def skill_set_users
-      params.require(:skill_set).permit(:user_ids=>[])
-    end
-
-    def ancestry_options(items)
-    result = []
-    items.map do |item, sub_items|
-      result << [yield(item), item.id]
-      #this is a recursive call:
-      result += ancestry_options(sub_items) {|i| "#{'-' * i.depth} #{i.name}" }
-    end
-    result
+  def skill_set_params
+    params.require(:skill_set).permit(:name,:parent_id,:user_ids=>[],chat_que_attributes: [:id,:name])
   end
+
+  def skill_set_users
+    params.require(:skill_set).permit(:user_ids=>[])
+  end
+    
+  
 end
